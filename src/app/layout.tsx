03a14a5009d33b11d63f24c8a83ed236a9584050
baptou-cloud <baptistetipsi@gmail.com>
2025-11-21 18:39:23 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-// src/app/layout.tsx
 import "./globals.css";
 import Header from "@/components/Header";
 import LegalFooter from "@/components/LegalFooter";
@@ -16,15 +15,10 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body className="bg-slate-50 min-h-screen flex flex-col">
-        {/* Seul et unique Header qui fait tout */}
         <Header />
-
-        {/* Contenu principal */}
         <main className="flex-1 pt-20 pb-24">
           {children}
         </main>
-
-        {/* Footer légal fixe en bas */}
         <LegalFooter />
       </body>
     </html>
