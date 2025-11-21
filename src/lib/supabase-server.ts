94cd457a02dc752cbfd482e16cf3edaf76c6d2e9
baptou-cloud <baptistetipsi@gmail.com>
2025-11-21 18:32:53 +0100

    Update supabase-server.ts

--- src/lib/supabase-server.ts
+++ src/lib/supabase-server.ts
@@ -1,16 +1,8 @@
 // src/lib/supabase-server.ts
-import { createServerClient } from "@supabase/auth-helpers-nextjs";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
 export const createServerClient = () =>
-  createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookies().get(name)?.value;
-        },
-      },
-    }
-  );
+  createServerComponentClient({
+    cookies,
+  });
