c4e0259a514f60f62bc2bf507957ef85e620a214
baptou-cloud <baptistetipsi@gmail.com>
2025-11-21 18:42:32 +0100

    Delete src/lib/supabase-server.ts

--- src/lib/supabase-server.ts
+++
@@ -1,8 +0,0 @@
-// src/lib/supabase-server.ts
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
-
-export const createServerClient = () =>
-  createServerComponentClient({
-    cookies,
-  });
